0f016dbf414fd7d81f5861c06db3fd9d6fde2235
legendtosif <toofanxd72@gmail.com>
2024-12-17 18:24:03 +0530

    Update nexio.py

--- SACHIN_MUSIC/plugins/sudo/nexio.py
+++ SACHIN_MUSIC/plugins/sudo/nexio.py
@@ -53,7 +53,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["exi","exiko"], prefixes=["n", "N"]) & admin_filter)
+@app.on_message(filters.command(["rose","roseie"], prefixes=["r", "R"]) & admin_filter)
 async def restriction_app(app :app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
