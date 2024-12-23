4ca133c0c89fd868d20d550c995ca2863aed2db7
ISTKHAR-king <istkharalam225@gmail.com>
2024-12-23 10:21:48 +0530

    Update reload.py

--- SACHIN_MUSIC/plugins/tools/reload.py
+++ SACHIN_MUSIC/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("tosu")
     & filters.private
-    & filters.user(7520092354)
+    & filters.user(7995626429)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
