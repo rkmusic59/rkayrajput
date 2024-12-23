2116fda8e46a7400cd94475304bafd0194df7459
legendtosif <toofanxd72@gmail.com>
2024-12-23 08:06:44 +0530

    Update unbanall.py

--- SACHIN_MUSIC/plugins/admins/unbanall.py
+++ SACHIN_MUSIC/plugins/admins/unbanall.py
@@ -4,7 +4,7 @@ from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from SACHIN_MUSIC.utils.SACHIN_BAN import admin_filter
 
-BOT_ID = "6754241147"
+BOT_ID = "7520092354"
 
 @app.on_message(filters.command("unbanall") & admin_filter)
 async def unban_all(_, msg):
