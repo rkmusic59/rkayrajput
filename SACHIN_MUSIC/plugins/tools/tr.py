f888391da530f119bc22c46814bb114e504e9e26
⎯𓆩꯭ 𝐀 ꯭ʟ ꯭ᴘ ꯭ʜ꯭ ᴧ꯭🥂꯭༎꯭ 𓆪꯭ <168528642+TEAMPURVI@users.noreply.github.com>
2025-01-18 23:52:50 +0530

    Update tr.py

--- SACHIN_MUSIC/plugins/tools/tr.py
+++ SACHIN_MUSIC/plugins/tools/tr.py
@@ -1,38 +1,21 @@
-from pyrogram import filters
-from pyrogram.types import *
 from SACHIN_MUSIC import app
-from deep_translator import Translator
+from pyrogram import filters
+from deep_translator import GoogleTranslator
 
-#.......
+@app.on_message(filters.command(["tr", "t"], prefixes=["/", "!", ".", ""]))
+async def translate(client, message):
+    if message.reply_to_message:
+        text_to_translate = message.reply_to_message.text
+        target_language = message.text.split(None, 1)[1] if len(message.text.split()) > 1 else 'en'
+    else:
+        if len(message.text.split()) < 3:
+            await message.reply_text("❖ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ʙʏ ʀᴇᴘʟʏɪɴɢ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ...")
+            return
+        target_language = message.text.split(None, 2)[1]
+        text_to_translate = message.text.split(None, 2)[2]
 
-trans = Translator()
-
-#......
-
-@app.on_message(filters.command("tr"))
-async def translate(_, message) -> None:
-    reply_msg = message.reply_to_message
-    if not reply_msg:
-        await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴛʀᴀɴsʟᴀᴛᴇ ɪᴛ !")
-        return
-    if reply_msg.caption:
-        to_translate = reply_msg.caption
-    elif reply_msg.text:
-        to_translate = reply_msg.text
     try:
-        args = message.text.split()[1].lower()
-        if "//" in args:
-            source = args.split("//")[0]
-            dest = args.split("//")[1]
-        else:
-            source = await trans.detect(to_translate)
-            dest = args
-    except IndexError:
-        source = await trans.detect(to_translate)
-        dest = "en"
-    translation = await trans(to_translate, sourcelang=source, targetlang=dest)
-    reply = (
-        f"ᴛʀᴀɴsʟᴀᴛᴇᴅ ғʀᴏᴍ {source} to {dest}:\n"
-        f"{translation.text}"
-    )
-    await message.reply_text(reply)
+        translated = GoogleTranslator(source='auto', target=target_language).translate(text_to_translate)
+        await message.reply_text(f"<b>ᴛʀᴀɴsʟᴀᴛᴇᴅ ғʀᴏᴍ</b> {target_language}\n\n<code>{translated}</code>")
+    except Exception as e:
+        await message.reply_text(f"An error occurred during translation: {str(e)}")
