a9fc45baad92e99671ae7ce1ca1119ad0abdf2c7
iamrkay786 <radhamusicbot5911@gmail.com>
2025-01-18 21:11:47 +0530

    Update gadd.py

--- SACHIN_MUSIC/plugins/tools/gadd.py
+++ SACHIN_MUSIC/plugins/tools/gadd.py
@@ -12,7 +12,7 @@ async def add_allbot(client, message):
     command_parts = message.text.split(" ")
     if len(command_parts) != 2:
         await message.reply(
-            "**❍ ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀᴍᴀᴛ. ᴘʟᴇᴀsᴇ ᴜsᴇ ʟɪᴋᴇ » `/gadd @zoyumusicbot`**"
+            "**❍ ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀᴍᴀᴛ. ᴘʟᴇᴀsᴇ ᴜsᴇ ʟɪᴋᴇ » `/gadd @radha_music_bot`**"
         )
         return
 
