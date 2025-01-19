2a9bd320d2ffc983af94aa99953e2506527ef237
iamrkay786 <radhamusicbot5911@gmail.com>
2025-01-19 08:58:32 +0530

    Update queue.py

--- SACHIN_MUSIC/utils/inline/queue.py
+++ SACHIN_MUSIC/utils/inline/queue.py
@@ -70,12 +70,12 @@ def aq_markup(_, chat_id):
             InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true&admin=delete_messages+invite_users",)
         ],
         [
-            InlineKeyboardButton(text="˹ ʀᴇsᴜᴍᴇ ˼", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="˹ ᴘᴧᴜsᴇ ˼", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="˹ ʀєsᴜᴍє ˼", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="˹ ᴘᴧᴜsє ˼", callback_data=f"ADMIN Pause|{chat_id}"),
         ],
         [
             InlineKeyboardButton(text="˹ sᴋɪᴘ ˼", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="˹ sᴛᴏᴘ ˼", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="˹ sᴛσᴘ ˼", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close",),
