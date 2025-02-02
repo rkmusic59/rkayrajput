9bd4601cb153a8f8d89be9c3eeee5e9eef292e5f
rkmusic59 <rkmusic5911@gmail.com>
2025-02-02 21:59:33 +0530

    Update autoleave.py

--- SACHIN_MUSIC/plugins/mics/autoleave.py
+++ SACHIN_MUSIC/plugins/mics/autoleave.py
@@ -8,38 +8,31 @@ from SACHIN_MUSIC import app
 from SACHIN_MUSIC.core.call import SACHIN, autoend
 from SACHIN_MUSIC.utils.database import get_client, is_active_chat, is_autoend
 
-
 async def auto_leave():
-    if config.AUTO_LEAVING_ASSISTANT == str(True):
-        while not await asyncio.sleep(
-            config.AUTO_LEAVE_ASSISTANT_TIME
-        ):
-            from SACHIN_MUSIC.core.userbot import assistants
+    if config.AUTO_LEAVING_ASSISTANT:
+        while not await asyncio.sleep(900):
+            from YTMUSIC.core.userbot import assistants
 
             for num in assistants:
                 client = await get_client(num)
                 left = 0
                 try:
-                    async for i in client.iter_dialogs():
-                        chat_type = i.chat.type
-                        if chat_type in [
-                            "supergroup",
-                            "group",
-                            "channel",
+                    async for i in client.get_dialogs():
+                        if i.chat.type in [
+                            ChatType.SUPERGROUP,
+                            ChatType.GROUP,
+                            ChatType.CHANNEL,
                         ]:
-                            chat_id = i.chat.id
                             if (
-                                chat_id != config.LOGGER_ID
-                                and i.chat.id != -1001919135283
-                                and i.chat.id != -1001841879487
+                                i.chat.id != config.LOGGER_ID
+                                and i.chat.id != -1001465277194
+                                and i.chat.id != -1002120144597
                             ):
                                 if left == 20:
                                     continue
-                                if not await is_active_chat(chat_id):
+                                if not await is_active_chat(i.chat.id):
                                     try:
-                                        await client.leave_chat(
-                                            chat_id
-                                        )
+                                        await client.leave_chat(i.chat.id)
                                         left += 1
                                     except:
                                         continue
@@ -52,7 +45,8 @@ asyncio.create_task(auto_leave())
 
 async def auto_end():
     while not await asyncio.sleep(5):
-        if not await is_autoend():
+        ender = await is_autoend()
+        if not ender:
             continue
         for chat_id in autoend:
             timer = autoend.get(chat_id)
